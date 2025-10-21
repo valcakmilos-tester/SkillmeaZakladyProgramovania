f03f42691a77c3bfc699be21e7ae2150f0c43745
Milos Valcak <valcak.milos@gmail.com>
2025-10-21 20:03:08 +0200

    scanner

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 /*
@@ -11,7 +13,28 @@ public class Main {
         System.out.println("Hello World!");
 */
 
+        // Kalkulacka
+      /*  int prveCislo = 10;
+        int druheCislo = 5;
+
+        System.out.println(prveCislo + druheCislo);
+        System.out.println(prveCislo - druheCislo);
+        System.out.println(prveCislo * druheCislo);
+        System.out.println(prveCislo / druheCislo);
+
+        boolean pravdaNepravda = false; */
+/* ---nefungovali mi desatinne cisla
+        float prveDesCislo = 0.1;
+        float druheDesCislo = 0.2;
+        System.out.println(prveDesCislo + druheDesCislo);
+  */
+  /*      System.out.println("Ahoj, zadaj meno: ");
+        Scanner  mojScanner = new Scanner(System.in);
+        String meno = mojScanner.nextLine();
+        System.out.println("Prajem pekny den " + meno);
+  */
 
 
     }
+   */
 }
