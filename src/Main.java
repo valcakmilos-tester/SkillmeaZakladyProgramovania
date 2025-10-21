f076048ec63853f3a1d3155acfbf52abacf6664c
Milos Valcak <valcak.milos@gmail.com>
2025-10-21 19:22:00 +0200

    zaklady

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+/*
+        String pozdrav = "Ahoj svet";
+        String meno = "praje Miloš";
+        meno = pozdrav;
 
+       // Toto je vypisanie konzoly
+        System.out.println(pozdrav + " " + meno);
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+*/
 
 
 
